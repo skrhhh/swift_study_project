e620af00a6f3457e430d4c32613abfcfbbe3bdcf
wuzhiwei <735596553@qq.com>
2024-06-19 14:15:32 +0800

    项目二

---
+++ Project2/Project2/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Project2
+//
+//  Created by admin on 2024/6/19.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
