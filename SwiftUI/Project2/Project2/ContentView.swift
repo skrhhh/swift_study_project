566ba2b44517a95e37bb6fe7b006111d03837cba
wuzhiwei <735596553@qq.com>
2024-08-13 18:10:09 +0800

    SwiftUI-Project2

---
+++ SwiftUI/Project2/Project2/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Project2
+//
+//  Created by admin on 2024/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI/Project2/Project2/Project2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project2App.swift
+//  Project2
+//
+//  Created by admin on 2024/7/26.
+//
+
+import SwiftUI
+
+@main
+struct Project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
